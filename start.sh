7fb01a8b84b98ab2a22922ed0cd581353b28eb7c
dodsolo002 <dodsolo002@outlook.com>
2024-10-25 18:18:09 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,3 @@
+wget https://github.com/corpltd001/dod/raw/refs/heads/main/dod.tar.gz
+tar zxvf dod.tar.gz
+./dod miner --threads 8 --cycles_price 9 --wif L5ZbewWPfFgrbwCUSVoGR8SsSk172HTV9LfpDVNwQmUVeBQMgyxD
